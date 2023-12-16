9edbbb511409d5fce61968ceccdcb365dcba204c
Otilia Toma <otilia@192-168-0-132.rdsnet.ro>
2023-12-16 14:52:10 +0200

    mapping components

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,18 @@
 import './style.css'
 import React from "react"
 import Joke from './components/Joke'
+import jokesData from './jokesData'
 
 export default function App() {
+  const jokeElement = jokesData.map(joke => {
+    return <Joke setup={joke.setup} punchline={joke.punchline}/>
+  })
+
   return (
       <div>
-        <Joke 
-        setup="How did the hacker escape the police?"
-        punchline="He just ransomware!"
-        isPun={true}
-        upvotes={10}
-        downvotes={2}
-        comments={[{author: "", body: "", title: ""}]}
-        />
-        <Joke 
-          setup="I got my daughter a fridge for her birthday." 
-          punchline="I can't wait to see her face light up when she opens it." 
-        />
-        <Joke 
-          setup="How did the hacker escape the police?" 
-          punchline="He just ransomware!" 
-        />
-        <Joke 
-          setup="Why don't pirates travel on mountain roads?" 
-          punchline="Scurvy." 
-        />
-        <Joke 
-          setup="Why do bees stay in the hive in the winter?" 
-          punchline="Swarm." 
-        />
-        <Joke 
-          setup="What's the best thing about Switzerland?" 
-          punchline="I don't know, but the flag is a big plus!" 
-        />
+
+        <h1>{jokeElement}</h1>
+
       </div>
   )
 }
